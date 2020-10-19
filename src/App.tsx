9c53b1c8bbfcf55a783604d5a30d890cdb38da6e
stosikmartyna <stosik.martyna@gmail.com>
2020-10-19 15:20:37 +0200

    Empty repository

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { MainView } from './views/MainView';
-import './App.css';
-
-export const App = () => {
-  return (
-    <MainView />
-  );
-}
--- src/views/MainView.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {Hero} from '../components/Hero/Hero';
-
-export const MainView = () => {
-    return <Hero />
-}
