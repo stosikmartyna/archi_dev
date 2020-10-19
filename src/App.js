40cac34f56ad133db81c084d7ea09e345d47c9a1
stosikmartyna <stosik.martyna@gmail.com>
2020-10-19 19:24:40 +0200

    Install firebase and implement user authentication (sign in)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import firebase from 'firebase';
 import { UserAuthView } from './views/UserAuthView';
 import './App.css';
 
 export const App = () => {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBnVf4mKXtz5d0dgQGX5nX_ydsUH4IqTFA",
+    authDomain: "archi-dev.firebaseapp.com",
+    databaseURL: "https://archi-dev.firebaseio.com",
+    projectId: "archi-dev",
+    storageBucket: "archi-dev.appspot.com",
+    messagingSenderId: "518315362308",
+    appId: "1:518315362308:web:f8ec564845b40de1a30d51"
+  });
+
   return (
     <UserAuthView />
   );
--- src/components/UserAuth/UserAuthSignIn.jsx
+++ src/components/UserAuth/UserAuthSignIn.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import firebase from 'firebase';
 import { authForm } from './UserAuth.styles';
+import { useHistory } from 'react-router-dom';
 
 export const UserAuthSignIn = () => {
+    const history = useHistory();
+
+    const handleSignIn = useCallback(
+        async (event) => {
+            event.preventDefault();
+            const { email, password } = event.target.elements;
+            try {
+                await firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                    history.push('/');
+            } 
+            catch (error) {
+                alert(error.message)
+            }
+        }, [history]
+    )
+
     return (
-        <form className={authForm}>
-            <label for={'email'}>
+        <form className={authForm} onSubmit={handleSignIn}>
+            <label htmlFor={'email'}>
                 Email
                 <input id={'email'} type={'text'} />
             </label>
-            <label for={'password'}>
+            <label htmlFor={'password'}>
                 Password
                 <input id={'password'} type={'password'} />
             </label>
-            <button>Sign In</button>
+            <button type={'submit'}>Sign In</button>
         </form>
     )
 }
