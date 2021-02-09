070e4bd571f61c0cbb08e520b0f15f278d959448
stosikmartyna <stosik.martyna@gmail.com>
2021-02-09 17:27:55 +0100

    Test: UserAuth / switch forms when click on text

--- src/test/integration/UserAuth.test.js
+++ src/test/integration/UserAuth.test.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { UserAuth } from '../../components/UserAuth/UserAuth';
 import '@testing-library/jest-dom/extend-expect'; 
+import userEvent from '@testing-library/user-event';
 
 describe('UserAuth component', () => {
-    it('displays logo correctly', () => {
-        const {getByRole} = render(<UserAuth />)
+    it('switch forms when click on text', () => {
+        const {getByRole, getByText, getAllByText} = render(<UserAuth />)
 
         const logo = getByRole('heading', { name: /archi dev/i });
-
         expect(logo).toBeInTheDocument();
+        
+        userEvent.click(getByText('Create account'));
+
+        expect(getAllByText('Sign Up')).toHaveLength(2);
+
+        userEvent.click(getByText('I already have an account'));
+
+        expect(getAllByText('Sign In')).toHaveLength(2);
+        expect(getByText('Create account')).toBeInTheDocument();
     });
 })
